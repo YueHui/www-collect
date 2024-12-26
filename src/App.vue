53d94f01ba22dfb661e3453cfbc9eac440951b6a
YueHui <527329923@qq.com>
2024-12-26 22:12:03 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
 
 <template>
     <a href="https://cy.collectapp.org" title="成语大全" target="_blank">成语大全</a><br/>
-    <a href="https://cy.collectapp.org" title="星座大全" target="_blank">星座大全</a>
+    <a href="https://xz.collectapp.org" title="星座大全" target="_blank">星座大全</a>
 </template>
 
 <style scoped>
