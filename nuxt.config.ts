d306f63a4c987182e96ad5be089ab9268c056832
YueHui <527329923@qq.com>
2025-03-08 11:07:53 +0800

    remove seo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,8 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: [
-    '@nuxtjs/tailwindcss',
-    '@nuxtjs/seo',
+    '@nuxtjs/tailwindcss'
   ],
   app: {
     head: {
